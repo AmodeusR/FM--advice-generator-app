1dc9d30141bd0a954b8c8e3b3956512c5c33f679
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-10-22 21:53:17 -0300

    Created index for assets

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import IconDice from "./icon-dice.svg";
+import MobileDivider from "./pattern-divider-mobile.svg";
+import DesktopDivider from "./pattern-divider-desktop.svg";
+
+export {
+  IconDice,
+  MobileDivider,
+  DesktopDivider
+}
