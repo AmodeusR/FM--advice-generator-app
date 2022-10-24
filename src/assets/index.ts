4a37aa0943d18dd091d943efd88abd8fd4761237
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-10-24 09:38:24 -0300

    Removed Icondice from assets index

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,7 @@
-import IconDice from "./icon-dice.svg";
 import MobileDivider from "./pattern-divider-mobile.svg";
 import DesktopDivider from "./pattern-divider-desktop.svg";
 
 export {
-  IconDice,
   MobileDivider,
   DesktopDivider
 }
